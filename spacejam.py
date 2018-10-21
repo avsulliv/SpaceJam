cc6650a2394c8df3399cdc3911a0bc1c50ca5004
Hugh_weinbridge <hugh_weinbridge@BigAls-MacBook-Pro.local>
2018-10-20 17:01:40 -0700

    updated Orbit class
    Added methods for computing angular momentum, period in date time
    object and extracting orbital parameters.

--- spacejam.py
+++ spacejam.py
@@ -1,12 +1,43 @@
 ### SPACEJAM.PY ###
 
 ### LIBRARIES ###
-import pyorbital
+from pyorbital.orbital import Orbital
+import datetime
+import numpy as np
 
 ### GLOBAL CONSTANTS ###
 
 ### GLOBAL FUNCTIONS ###
 
+def segment_tle_list(filename):
+	fp = open(filename,'r')
+
+def next_tle(filepointer):
+	x = list()
+	for i in range(3):
+		x.append(filepointer.readline())
+	return x
+
+def compare_orbits(orbit_A, orbit_B):
+	return
 ### CLASSES ###
+
+class Orbit(Orbital):
+	'''Wrapper class for the pyorbital.orbital.Orbital class plus additional methods.'''
+	#(r,v)=ISS.get_position(now,False)
+	def angular_momentum(self,dt):
+		(r,v)=self.get_position(dt,False) 	#r is radius vector in meters and v is velocity vector in km/s.
+		self.h = np.cross(r,v*1000) 		#h is angular momentum vector in m^2/s
+
+	def _compute_orbital_parameters(self):
+		self.Omega = self.tle.right_ascension
+		self.i = self.tle.inclination
+		self.e = self.tle.excentricity
+		self.omega = self.tle.arg_perigee
+		self.M = self.tle.mean_anomaly
+	def _compute_period(self):
+		self.period = datetime.timedelta(minutes=self.orbit_elements.period)
+		return self.period.total_seconds()
+
 
 ### END OF SPACEJAM.PY ###
